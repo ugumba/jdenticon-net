9c5d07bb809979541b142bbb009ac2501dc5ce31
Robert Schmidt <robertsc@cegal.com>
2017-06-11 13:46:03 +0200

    update for VS2017

--- Jdenticon/Properties/AssemblyInfo.cs
+++ Jdenticon/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Jdenticon-net")]
-[assembly: AssemblyDescription("Jdenticon is a library for generating highly recognizable identicons.")]
+[assembly: AssemblyDescription("Jdenticon is a library for generating highly recognizable identicons.\r\nCegal.Jdenticon is a custom fork by Cegal AS, built for .NET Core.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Daniel Mester Pirttijärvi")]
 [assembly: AssemblyProduct("Jdenticon")]
